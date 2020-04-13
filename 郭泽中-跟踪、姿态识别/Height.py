1e7ee981ab33279eae0fcee76c90c31b7d7d57a4
郭泽中 <guozzeof@163.com>
2020-04-13 14:06:11 +0800

    将身高计算添加偏差部分整合到了杨佳辉那里

--- 郭泽中-跟踪、姿态识别/Height.py
+++ 郭泽中-跟踪、姿态识别/Height.py
@@ -1,6 +1,4 @@
 import numpy as np
-import pickle
-from sklearn.linear_model import LinearRegression
 
 '''
     算法思想：
@@ -12,18 +10,8 @@ from sklearn.linear_model import LinearRegression
 
 class Height():
 
-    block_length=0.25
-    # delta_height=[1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.2599404864574784, 1.1344320084145112,
-    #               1.1988682786418468, 1.1119083755798245, 0.9852783189205492, 0.7072995609122058, 0.6282585815117643,
-    #               0.3319765558510601, 0.08778045385836619, 0.008194747981847827, -0.25343017749817864,
-    #               -0.37420679559517334, -0.4872027432969972, -0.6316345401074752, -0.701356690112346,
-    #               -0.875, -1.05, -1.225, -1.4024673937450403, -1.5452798970619157]
-    delta_height=[1.35, 1.35, 1.35, 1.35, 1.35, 1.35, 1.35, 1.35, 1.28, 1.22, 1.24, 1.06, 0.81, 0.67, 0.55, 0.47, 0.18,
-                  0.04, -0.22, -0.43, -0.52, -0.58, -0.86, -1, -1.14, -1.3, -1.46, -1.44]
-
-
-    def __init__(self,height,distance):
-        self.height=[self.add_delta(height,distance)]
+    def __init__(self,height):
+        self.height=[height]
         self.origin_height=[height]
 
         #kalman部分
@@ -53,26 +41,13 @@ class Height():
         self.P=(1-K)*self.P_
 
 
-    def add_height(self,height,distance):
-        # self.predict()
-        # self.update(height)
+    def add_height(self,height):
         self.origin_height.append(self.s)
 
-        real_height=self.add_delta(height,distance)
+        self.predict()
+        self.update(height)
 
-        self.height.append(real_height)
+        self.height.append(self.s)
 
         if len(self.height)>100:
             del self.height[0]
-
-    # def add_real_height(self,height):
-    #     self.origin_height.append(height)
-    #     self.height.append(height)
-    #
-    #     if len(self.height)>100:
-    #         del self.height[0]
-
-    def add_delta(self,height,distance):
-        if int(distance/self.block_length)>=len(self.delta_height):
-            return height+self.delta_height[-1]
-        return height+self.delta_height[int(distance/self.block_length)]
